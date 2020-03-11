6e2a7f9b102299a5f4daf297e5055b2dbddbbb3e
kevmeath <kevmeath@gmail.com>
2020-03-11 21:58:07 +0000

    Lab 1

--- Lab4/ValidatedAccount.java
+++
@@ -1,6 +0,0 @@
-package com.oop.lab4;
-
-public interface ValidatedAccount {
-	void getDetails();
-	void valuableAccount();
-}
--- Lab5/Control.java
+++
@@ -1,9 +0,0 @@
-package com.oop.lab5;
-
-public class Control {
-
-	public static void main(String[] args) {
-		Screen s1 = new Screen("Title");
-	}
-
-}
